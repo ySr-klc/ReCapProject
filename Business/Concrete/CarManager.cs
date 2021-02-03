34efb48d9348e42a8632460ddf56149440e3f412
yasir kılıç <nfsworld196219@hotmail.com>
2021-02-03 19:53:58 +0300

    Add project files.

---
+++ Business/Concrete/CarManager.cs
@@ -0,0 +1,28 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using DataAccess.Concrete;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CarManager : ICarService
+    {
+
+        ICarDal _CarDal;
+
+        public CarManager(ICarDal inMemoryCarDal)
+        {
+            _CarDal = inMemoryCarDal;
+
+           
+        }
+
+        public List<Car> GetAll()
+        {
+            return _CarDal.GetAll();
+        }
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,19 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+     class Program //Arkadaşlar kusura bakmayın projede dictionary kısmı eksik,yetiştiremedim.Yapısal olarak çalışmaktadır.  İlerleyen zamanlarda bitirmeyi düşünüyorum.
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+
+            foreach  (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.CarId );
+            }
+        }
+    }   
+}
