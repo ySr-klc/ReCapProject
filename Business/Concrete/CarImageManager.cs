43db72b4389194a93c20bcab80831f67d0ee455e
yasir kılıç <nfsworld196219@hotmail.com>
2021-02-27 03:44:44 +0300

    -

---
+++ Business/Concrete/CarImageManager.cs
@@ -0,0 +1,57 @@
+﻿using Business.Abstract;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CarImageManager : ICarService
+    {
+
+        ICarDal _carDal;
+
+        public CarImageManager(ICarDal carDal)
+        {
+            _carDal = carDal;
+        }
+
+        public IResult Add(Car car)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult Delete(Car car)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<List<Car>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<List<Car>> GetByBrandId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<List<Car>> GetByColorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<List<CarDetailDto>> GetCarDetailDtos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult Update(Car car)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
